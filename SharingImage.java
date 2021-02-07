19f11d0da0557164294b19d2f641a1cdc693b78a
MandeepLamba <mandeeplamba225@gmail.com>
2021-02-07 20:21:29 +0530

    WhatsApp sharing added

--- SharingImage.java
+++ SharingImage.java
@@ -11,82 +11,122 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Build;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.core.content.ContextCompat;
 
 public class SharingImage {
-    private static final int WRITE_EXTERNAL_PERMISSION_REQUEST_CODE = 3434;
+    public static final int WRITE_EXTERNAL_PERMISSION_REQUEST_CODE = 34344;
     private Context context;
+    private static final String TAG = "SharingImage";
 
 
-    public void shareImage(View view){
-        Bitmap result = Bitmap.createBitmap(view.getWidth(),view.getHeight(),Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(result);
-        Drawable drawable = view.getBackground();
-        if(drawable != null){
-            drawable.draw(canvas);
-        }
-        else{
-            canvas.drawColor(Color.WHITE);
-        }
-        view.draw(canvas);
-        if(checkPermission()){
-            startSharing(result);
-        }
+    public void shareImage(View view) {
+        if (view != null) {
+            if (checkPermission()) {
+                Bitmap result = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas(result);
+                Drawable drawable = view.getBackground();
+                if (drawable != null) drawable.draw(canvas);
+                else canvas.drawColor(Color.WHITE);
+                view.draw(canvas);
+                startSharing(result);
+            }
+        } else Log.d(TAG, "shareImage: View is null sharing Failed");
+    }
+    public void shareImage(View view, String message) {
+        if (view != null) {
+            if (checkPermission()) {
+                Bitmap result = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas(result);
+                Drawable drawable = view.getBackground();
+                if (drawable != null) drawable.draw(canvas);
+                else canvas.drawColor(Color.WHITE);
+                view.draw(canvas);
+                startSharing(result, message);
+            }
+        } else Log.d(TAG, "shareImage: View is null sharing Failed");
+    }
+
+    public void shareImageToWhatsApp(View view, String message){
+        if (view != null) {
+            if (checkPermission()) {
+                try {
+                    context.getPackageManager().getPackageInfo("com.whatsapp",0);
+                    Bitmap result = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+                    Canvas canvas = new Canvas(result);
+                    Drawable drawable = view.getBackground();
+                    if (drawable != null) drawable.draw(canvas);
+                    else canvas.drawColor(Color.WHITE);
+                    view.draw(canvas);
+                    startSharing(result, message, "com.whatsapp");
+                } catch (PackageManager.NameNotFoundException e) {
+                    e.printStackTrace();
+                    Toast.makeText(context, "WhatsApp is not installed", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        } else Log.d(TAG, "shareImage: View is null sharing Failed");
     }
 
     private boolean checkPermission() {
         if (ContextCompat.checkSelfPermission(
-                context, Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
-                PackageManager.PERMISSION_GRANTED) {
-            // You can use the API that requires the permission.
+                context, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "checkPermission: Permissions are Granted");
             return true;
-        } else if (((Activity)context).shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-            // In an educational UI, explain to the user why your app requires this
-            // permission for a specific feature to behave as expected. In this UI,
-            // include a "cancel" or "no thanks" button that allows the user to
-            // continue using your app without granting the permission.
-            new AlertDialog.Builder(context)
-                    .setTitle("Permission Required")
-                    .setMessage("Write Permission is required to create and share profile badges")
-                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            ((Activity) context).requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},WRITE_EXTERNAL_PERMISSION_REQUEST_CODE);
-                        }
-                    })
-                    .setNegativeButton("No thanks", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            Toast.makeText(context, "Permission Denial", Toast.LENGTH_SHORT).show();
-                        }
-                    }).show();
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (((Activity) context).shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                new AlertDialog.Builder(context).setTitle("Permission Required")
+                        .setMessage("Write Permission is required to create and share profile badges")
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                ((Activity) context).requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_EXTERNAL_PERMISSION_REQUEST_CODE);
+                            }
+                        })
+                        .setNegativeButton("No thanks", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(context, "Permission Denial", Toast.LENGTH_SHORT).show();
+                            }
+                        }).show();
 
-        } else {
-            // You can directly ask for the permission.
-            // The registered ActivityResultCallback gets the result of this request.
-            ((Activity) context).requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},WRITE_EXTERNAL_PERMISSION_REQUEST_CODE);
+            } else
+                ((Activity) context).requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_EXTERNAL_PERMISSION_REQUEST_CODE);
         }
         return false;
     }
 
 
-    private void startSharing(Bitmap bitmap){
+    private void startSharing(Bitmap bitmap) {
         String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), bitmap, "Profile Badge", "My Profile Badge");
-        Uri uri = Uri.parse(path);
         Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("image/jpeg");
-        intent.putExtra(Intent.EXTRA_STREAM,uri);
+        intent.putExtra(Intent.EXTRA_STREAM, Uri.parse(path)).setType("image/*");
         context.startActivity(intent);
     }
 
+    private void startSharing(Bitmap bitmap,String message) {
+        String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), bitmap, "Profile Badge", "My Profile Badge");
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_TEXT,message);
+        intent.putExtra(Intent.EXTRA_STREAM, Uri.parse(path)).setType("image/*");
+        context.startActivity(intent);
+    }
+    private void startSharing(Bitmap bitmap,String message, String pack) {
+        String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), bitmap, "Profile Badge", "My Profile Badge");
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setPackage(pack);
+        intent.putExtra(Intent.EXTRA_TEXT,message);
+        intent.putExtra(Intent.EXTRA_STREAM, Uri.parse(path)).setType("image/*");
+        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        context.startActivity(intent);
+    }
 
-    public SharingImage(Context context){
+    public SharingImage(Context context) {
         this.context = context;
     }
 
@@ -94,7 +134,7 @@ public class SharingImage {
 //    @Override
 //    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
 //        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-//        if(requestCode == 3434){
+//        if(requestCode == SharingImage.WRITE_EXTERNAL_PERMISSION_REQUEST_CODE){
 //            if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
 //                Toast.makeText(this, "Permission Granted Successfully", Toast.LENGTH_SHORT).show();
 //            }
